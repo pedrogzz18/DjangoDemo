5a55f043469d1f6b9623a20582070f87a4ef94e8
Humberto Barrera Martínez <117108901+HumbertoBM2@users.noreply.github.com>
2023-11-14 23:30:40 -0600

    Update settings.py

--- pia/settings.py
+++ pia/settings.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 from pathlib import Path
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c&7h$!_supaa#%k!hv9jwj8y%9miel2d=swt%i)ttz_e0!paan'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default_value_if_not_set')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
