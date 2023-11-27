7ea7cac02865f59b40619d060b6d5bb3467920db
pedrogzz18 <pedro.gonzalezot@uanl.edu.mx>
2023-11-27 04:11:43 -0600

    make username for readers unique

--- ourbooks/models.py
+++ ourbooks/models.py
@@ -41,4 +41,4 @@ class Reader(models.Model):
     )
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    username = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
